ab1f592f9a529eafa43603789cc837af8973c773
João Pedro  Lima <69178897+JpLimags@users.noreply.github.com>
2022-05-08 20:45:04 -0300

    Add files via upload

---
+++ Lista3/q07.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define TAM 4
+#define TAMV 4
+
+int main(){
+
+    float mediaGeo, mediaAri;
+    int soma = 0, Vnums[TAM],produto = 1, j;
+    srand(time(NULL));
+
+    
+    for(int i = 0; i < TAMV; i++){
+        Vnums[i] = (rand() % (19 + 1 - 0));
+        //printf("%d\n",Vnums[i]);
+    }
+
+    for(int j = 0; j < TAMV; j++){
+
+        soma += Vnums[j];
+        produto *= Vnums[j];
+    }
+
+        mediaAri = (float) soma/TAM;
+        mediaGeo = pow(produto,(1.0/TAMV));
+
+    printf("A media aritmetica foi: %2.f\n A media geometrica foi: %.2f", mediaAri, mediaGeo);
+    return 0;
+}
